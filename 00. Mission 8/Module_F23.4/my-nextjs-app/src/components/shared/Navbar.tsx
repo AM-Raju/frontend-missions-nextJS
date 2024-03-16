1566efc5c94dd838527c66d6f12a6a7416686653
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-03-16 17:30:59 +0600

    Module F23.9 done - Navigation With Link Component & Create Built-In Routes

--- 00. Mission 8/Module_F23.4/my-nextjs-app/src/components/shared/Navbar.tsx
+++ 00. Mission 8/Module_F23.4/my-nextjs-app/src/components/shared/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
@@ -9,7 +10,7 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+        <li><Link href="/about">About</Link></li>
         <li>
           <a>Parent</a>
           <ul className="p-2">
@@ -24,7 +25,7 @@ const Navbar = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      <li><Link href="/about">About</Link></li>
       <li>
         <details>
           <summary>Parent</summary>
@@ -34,7 +35,7 @@ const Navbar = () => {
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
+      <li><Link href="/products">Product</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
