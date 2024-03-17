41e62b1142bda4bdc57236fe48eeec9630c4554e
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-03-17 09:31:32 +0600

    Module F24.3 done - Use Metadata For Better SEO And Import Alias

--- 00. Mission 8/Module_F24/f24-nextjs-app/src/app/page.jsx
+++ 00. Mission 8/Module_F24/f24-nextjs-app/src/app/page.jsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-import React from "react";
+export const metadata = {
+  title: "Home page",
+  description: "This is home page",
+};
 
 const HomePage = () => {
   return (
