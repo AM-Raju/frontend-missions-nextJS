3427206ad200687ae516b4b039ff77bab7622cf0
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-03-20 08:21:01 +0600

    M F25.9 done - Implement Loading UI (React Suspense) For Blogs

--- 00. Mission 8/Module_F25/blogiz/src/app/blogs/loading.tsx
+++ 00. Mission 8/Module_F25/blogiz/src/app/blogs/loading.tsx
@@ -1,7 +1,16 @@
-const BlogLoadingPage = () => {
+import LoadingCard from "@/components/ui/LoadingCard";
+import { IBlog } from "@/types/index.types";
+
+const BlogLoadingPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
   return (
     <div>
-      <h1 className="text-red-500 text-center text-5xl">...Loading</h1>
+      <div>
+        {blogs.map((blog: IBlog) => (
+          <LoadingCard key={blog.id}></LoadingCard>
+        ))}
+      </div>
     </div>
   );
 };
