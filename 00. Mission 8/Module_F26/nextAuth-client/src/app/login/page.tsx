23f8a41a23cb3246b7eeb4295ed89f6f9181536f
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-03-21 09:41:43 +0600

    f26.9-client - Login User Using Next.Js Server Actions And A Custom Server

--- 00. Mission 8/Module_F26/nextAuth-client/src/app/login/page.tsx
+++ 00. Mission 8/Module_F26/nextAuth-client/src/app/login/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { loginUser } from "@/utils/action/loginUser";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
-type FormValues = {
+export type FormValues = {
   email: string;
   password: string;
 };
@@ -16,8 +18,22 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
+  const router = useRouter();
+
   const onSubmit = async (data: FormValues) => {
     console.log(data);
+
+    try {
+      const res = await loginUser(data);
+      // console.log(res);
+      if (res.accessToken) {
+        alert(res.message);
+        localStorage.setItem("access-token", res.accessToken);
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
