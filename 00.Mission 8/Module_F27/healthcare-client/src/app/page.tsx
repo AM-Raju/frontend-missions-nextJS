cfe252bd0dfe6aa3e502f6d438c55f109dbe4ea4
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-03-31 09:03:39 +0600

    Moduel F28.1 and F28.2

---
+++ 00.Mission 8/Module_F27/healthcare-client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is homepage</h1>
+      <Button variant="outlined">Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
