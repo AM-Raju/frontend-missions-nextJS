9a66b3f56f41c8b09d2d6d3ad551544ecf26e9f3
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-01 10:34:11 +0600

    A little fixation.

--- 00.Mission 8/Module_F27/healthcare-client/src/components/shared/navbar/Navbar.tsx
+++ 00.Mission 8/Module_F27/healthcare-client/src/components/shared/navbar/Navbar.tsx
@@ -1,15 +1,33 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <Container>
-      <Typography variant="h5" component="h1" fontWeight={600}>
-        P
-        <Box component="span" color="primary.main">
-          H
-        </Box>{" "}
-        Health Care
-      </Typography>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="h5" component="h1" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H
+          </Box>{" "}
+          Health Care
+        </Typography>
+        <Stack direction="row" gap={4} justifyContent="space-between">
+          <Typography>Consultation</Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>Contact Us</Typography>
+        </Stack>
+        <Button component={Link} href="/login">
+          Login
+        </Button>
+      </Stack>
     </Container>
   );
 };
