c95e5c6e82c8b6e19d9516add3aa6598c49779a5
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-03 11:46:13 +0600

    F28.7, F28.8, F28.9, F28.10, F28.11, and Tasks

--- 00.Mission 8/Module_F27/healthcare-client/src/app/(withCommonLayout)/page.tsx
+++ 00.Mission 8/Module_F27/healthcare-client/src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,20 @@
 import HeroSection from "@/UI/homePage/heroSection/HeroSection";
+import HowItWorks from "@/UI/homePage/howItWorks/HowItWorks";
+import Specialist from "@/UI/homePage/specialist/Specialists";
+import Stats from "@/UI/homePage/stats/Stats";
+import TopRatedDoctors from "@/UI/homePage/topRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/UI/homePage/whyUs/WhyUs";
 import { Button, Container } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection></HeroSection>
+      <Specialist></Specialist>
+      <TopRatedDoctors></TopRatedDoctors>
+      <WhyUs></WhyUs>
+      <HowItWorks></HowItWorks>
+      <Stats></Stats>
     </>
   );
 };
