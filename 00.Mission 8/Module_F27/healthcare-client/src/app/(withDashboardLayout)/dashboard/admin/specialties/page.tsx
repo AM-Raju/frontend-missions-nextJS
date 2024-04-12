d3afe7c58f088bf25eb7c8c9853daf30517552bd
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-12 08:37:39 +0600

    F33.1 - Start Developing Specialist Feature In Admin Dashboard

--- 00.Mission 8/Module_F27/healthcare-client/src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ 00.Mission 8/Module_F27/healthcare-client/src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,10 +1,16 @@
+import PHModal from "@/components/shared/phModal/PHModal";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import React from "react";
 
 const SpecialtiesPage = () => {
   return (
-    <div>
-      <h1>This is specialties page</h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Create Specialty</Button>
+        <PHModal></PHModal>
+        <TextField size="small" placeholder="Search Specialist" />
+      </Stack>
+    </Box>
   );
 };
 
