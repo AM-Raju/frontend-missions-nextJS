34c3d92e51765b5f668ec0bb6e6c1068ac2b5f85
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-17 16:44:42 +0600

    F34.10- Modify and Display Schedule Data. Module Summary

--- 00.Mission 8/Module_F27/healthcare-client/src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
+++ 00.Mission 8/Module_F27/healthcare-client/src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -1,14 +1,75 @@
 "use client";
-import { Box, Button } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Button, IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import ScheduleModal from "./components/ScheduleModal";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
+import { ISchedule } from "@/types/schedule/schedule.types";
+import { dateFormatter } from "@/utils/dateFormatter";
+import dayjs from "dayjs";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 const SchedulesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [allSchedule, setAllSchedule] = useState<any>([]);
+  const { data, isLoading } = useGetAllSchedulesQuery({});
+
+  const schedules = data?.schedules;
+  const meta = data?.meta;
+
+  // console.log("Schedule page", schedules);
+
+  useEffect(() => {
+    const updateData = schedules?.map((schedule: ISchedule) => {
+      return {
+        id: schedule?.id,
+        startDate: dateFormatter(schedule.startDate),
+        endDate: dateFormatter(schedule.endDate),
+        startTime: dayjs(schedule?.startDate).format("hh:mm a"),
+        endTime: dayjs(schedule?.endDate).format("hh:mm a"),
+      };
+    });
+    setAllSchedule(updateData);
+  }, [schedules]);
+
+  const columns: GridColDef[] = [
+    { field: "startDate", headerName: "Start Date", flex: 1 },
+    { field: "endDate", headerName: "End Date", flex: 1 },
+    { field: "startTime", headerName: "Start Time", flex: 1 },
+    { field: "endTime", headerName: "End Time", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <IconButton aria-label="delete">
+              <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+            <IconButton aria-label="delete">
+              <EditIcon sx={{}} />
+            </IconButton>
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
     <Box>
       <Button onClick={() => setIsModalOpen(true)}>Create Schedule</Button>
       <ScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={allSchedule ?? []} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
     </Box>
   );
 };
